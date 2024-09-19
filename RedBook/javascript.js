593ed767df6429c65c96c5987f28dc0a2dc51913
Ekaterina <144220191+kkatrinna@users.noreply.github.com>
2024-09-19 12:21:29 +0300

    Update javascript.js

--- RedBook/javascript.js
+++ RedBook/javascript.js
@@ -1,3 +1,25 @@
 function navigateTo(url) {
   window.location.href = url;
 }
+
+function navigateTo(url) {
+  window.location.href = url;
+}
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+        center: [55.757197, 37.427884], 
+        zoom: 20
+    });
+
+    var placemark1 = new ymaps.Placemark([55.774602, 37.374917], {
+        balloonContent: 'Ёж, гадюка, уж, кутора, зелёная лягушка, травянка толстоголовая, скакун полевой, жужелица гладкая'
+    });
+
+	
+	
+
+
+    myMap.geoObjects.add(placemark1);
+    myMap.geoObjects.add(placemark2);
+});
